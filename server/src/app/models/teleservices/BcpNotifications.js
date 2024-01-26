7aa4e174394253328c6f08cebde7eb1e075596f7
NelsonChoque25 <147645097+NelsonChoque25@users.noreply.github.com>
2024-01-25 19:10:32 -0500

    Se realizo modulos para la extraccion de datos BCP

--- server/src/app/models/teleservices/BcpNotifications.js
+++ server/src/app/models/teleservices/BcpNotifications.js
@@ -1,70 +1,67 @@
-const { Model, DataTypes } = require('sequelize');
-const sequelize = new Sequelize('database', 'username', 'password', {
-  host: 'localhost',
-  dialect: /* one of 'mysql' | 'mariadb' | 'postgres' | 'mssql' */
-});
+const { Sequelize, DataTypes, Model } = require("sequelize");
+const sequelize = require("../../../config/database");
 
 class BcpNotification extends Model {}
 
-BcpNotifications.init({
-  id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    autoIncrement: true,
-    primaryKey: true,
+BcpNotification.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    operationType: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    operationDate: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    operationNumber: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    orderingCompany: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    account: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    accountType: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    beneficiary: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    destinationAccount: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    amount: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    reference: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    message: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
   },
-  operationType: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  transferToThirdParty: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  operationDate: {
-    type: DataTypes.DATE,
-    allowNull: false,
-  },
-  operationNumber: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  orderingCompany: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  account: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  accountType: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  beneficiary: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  destinationAccount: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  amount: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  reference: {
-    type: DataTypes.STRING,
-    allowNull: true,
-  },
-  message: {
-    type: DataTypes.STRING,
-    allowNull: true,
-  },
-}, {
-  sequelize,
-  modelName: 'BcpNotification',
-  tableName: 'BcpNotifications',
-});
+  {
+    sequelize,
+    modelName: "BcpNotification",
+    tableName: "BcpNotifications",
+    timestamps: false,
+  }
+);
 
-module.exports = BcpNotifications;
+module.exports = BcpNotification;
